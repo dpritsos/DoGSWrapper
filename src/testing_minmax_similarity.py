5d8ad6921ef3a43f8b3b34266b9e22a7a8c0855c
Dimitrios Pritsos <dpritsos@extremepro.gr>
2014-07-31 20:48:43 +0300

    Running Experiments for Jaccared Simil.

--- src/testing_minmax_similarity.py
+++ src/testing_minmax_similarity.py
@@ -4,11 +4,11 @@ import numpy as np
 
 def minmax_similarity(v1, v2):
 
-	return np.sum( np.min( np.vstack((v1,v2)), axis=0 ) ) / np.sum( np.max( np.vstack((v1,v2)), axis=0 ) )
+	return np.sum( np.min( np.vstack((v1,v2)), axis=0 ) ) / float(np.sum( np.max( np.vstack((v1,v2)), axis=0 ) ))
 
 
-c = np.array([1,1,1,0.01])
-rv = np.array([0.1,0.9,0.1,0.1])
+c = np.array([1,1,1,1,1,1])
+rv = np.array([1,1,1,0,0,1])
 
-print np.min( np.vstack((c,rv)), axis=0 ) 
+#print np.min( np.vstack((c,rv)), axis=0 ) 
 print minmax_similarity(c, rv)
