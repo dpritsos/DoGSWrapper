1d3b00f4e4c63d579fdb77c85a7708f536ceaea3
Dimitrios Pritsos <dpritsos@extremepro.gr>
2015-11-17 08:27:47 +0200

    Changing the code cycles timing method...

--- dogswrapper/wrappedmodels/HMRFKmeans.py
+++ dogswrapper/wrappedmodels/HMRFKmeans.py
@@ -62,11 +62,9 @@ class HMRFKmeans_Wrapped(object):
         # k-clusters expected, Must-Link and Cannot-Link constraints.
 
         self.hkmeans = HMRFKmeans(
-            k_clusters,  must_lnk, cannot_lnk, init_centroids=init_centrs,
+            k_clusters, must_lnk, cannot_lnk, init_centroids=init_centrs,
             max_iter=params['max_iter'], cvg=params['converg_diff'],
-            lrn_rate=params['learing_rate'], ray_sigma=0.8,
-            w_violations=np.random.uniform(
-                0.99, 0.99, size=(corpus_mtrx.shape[0], corpus_mtrx.shape[0])),
+            lrn_rate=params['learing_rate'], ray_sigma=1.0,
             d_params=np.random.uniform(1.0, 1.0, size=corpus_mtrx.shape[1]),
             norm_part=False, globj='non-normed'
         )
