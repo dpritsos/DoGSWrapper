3e0b28366f5adddfd50b288f263d709bdef16396
Dimitrios Pritsos <dpritsos@extremepro.gr>
2018-08-14 20:00:49 +0300

    Getting Ready for NNRD experiments

--- OpenSet_Gensim_MU.py
+++ OpenSet_Gensim_MU.py
@@ -14,24 +14,30 @@ import html2vec.tables.wngrams as h2v_wng
 from dogswrapper.evalmethods.openset import OpenSetParamGridSearchTables
 import dogswrapper.evalmethods.openness as openness
 from dogswrapper.tools.normalisers import MaxNormalise, SubSamplingNorm
-from dogswrapper.wrappedmodels import ocsvme, rfse
+from dogswrapper.wrappedmodels import ocsvme, rfse, nnrd
 
 
 # Santini's 7-genres Corpus
-corpus_filepath = "/mnt/turnstone/KI-04/"
-state_saving_path = "/mnt/turnstone/KI-04/" +\
-    "Openness_W3G_KI04/"
+corpus_filepath = "/home/dimitrios/Synergy-Crawler/SANTINIS/"
+state_saving_path = "/home/dimitrios/Synergy-Crawler/SANTINIS/" +\
+    "Open_W3G_SANTINIS/"
 if not os.path.exists(state_saving_path):
     os.mkdir(state_saving_path)
 
 # genres = ["blog", "eshop", "faq", "frontpage", "listing", "php", "spage"]
+# genres = [
+#   "article", "discussion", "download", "help", "linklist", "portrait",
+#   "portrait_priv", "shop"
+# ]
+
 genres = [
-    "article", "discussion", "download", "help", "linklist", "portrait", "portrait_priv", "shop"
+    "blog", "eshop", "faq", "frontpage", "listing", "php", "spage", "diy_mini",
+    "editorial", "feat_articles", "short_bio", "spirit_1000"
 ]
 
 # Creating or opeding existing file for saving the results.
 method_results = tb.open_file(
-    state_saving_path + 'OpenSet_MarkedUknown_OCSVME_W3G_Gensim_KI04_2018_07_22.h5', 'a'
+    state_saving_path + 'OpenSet_MarkedUknown_NNRD_W1G_Gensim_SANTINIS_2018_08_17.h5', 'a'
 )
 
 params_range = coll.OrderedDict([
@@ -45,31 +51,32 @@ params_range = coll.OrderedDict([
     ('min_alpha', [0.025]),
     ('epochs', [1, 3, 10]),
     ('decay', [0.002, 0.02]),
+    # openness
     # ('uknw_ctgs_num', [1, 2, 3, 4, 5, 6, 7]),
     # ('uknw_ctgs_num_splt_itrs', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]),
-    ('features_size', [25, 50, 100]),
-    ('sim_func', ['cosine_sim']),
-    ('Sigma', [0.5, 0.7, 0.9]),
-    ('Iterations', [10, 50, 100]),
+    # RFSE
+    # ('features_size', [25, 50, 100]),
+    # ('sim_func', ['minmax_sim']),
+    # ('Sigma', [0.5, 0.7, 0.9]),
+    # ('Iterations', [10, 50, 100]),
+    # OCSVME
     # ('nu', [0.05, 0.07, 0.1, 0.15, 0.17, 0.3, 0.5, 0.7, 0.9]),
-    ('marked_uknw_ctg_lst', [1]),
+    # NNRD
+    ('split_ptg', [0.7, 0.5]),
+    ('ukwn_slt_ptg', [0.3, 0.5]),
+    ('rt_lims_stp', [[0.8, 1.0, 0.2]]),
+    ('lmda', [0.2, 0.5, 0.7]),
+    ('marked_uknw_ctg_lst', [12]),
     ('kfolds', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
 ])
 
 word_n_gram_size = 3
-#tables_wng = h2v_wng.Html2TF(
 tables_wng = h2v_wng.Html2GsmVec(
+# tables_cng = h2v_cng.Html2GsmVec(
     word_n_gram_size, html_attrib=["text"], str_case='lower', valid_html=False
 )
 
-# char_n_gram_size = 4
-
-# tables_cng = h2v_cng.Html2TF(
-# tables_cng = h2v_cng.Html2GsmVec(
-#     char_n_gram_size, html_attrib=["text"], str_case='lower', valid_html=False
-# )
-
-openness_model = rfse
+openness_model = nnrd
 # openness_model = OCSVMEDMPG_Wrapped(genres)
 
 openset_unoise_searchgrid = OpenSetParamGridSearchTables(
@@ -98,7 +105,7 @@ openness_searchgrid.build_corpusmatrix_on_dlparams()
 results_h5 = openset_unoise_searchgrid.evaluate_on_open_unstrd_noise_iset()
 
 print results_h5
-print "OCSVME Experiments Done!"
+print "Experiments Done!"
 
 
 method_results.close()
--- dogswrapper/wrappedmodels/nnrd.py
+++ dogswrapper/wrappedmodels/nnrd.py
@@ -16,6 +16,8 @@ def eval(*args):
     cls_gnr_tgs = args[3]
     params = args[4]
 
+    print "Passed"
+
     # Initilising the NN model.
     osnndr = onndr.OpenNNDR(
         slt_ptg=params['split_ptg'],
@@ -24,11 +26,15 @@ def eval(*args):
         lmda=params['lmda']
     )
 
+    print "Passed"
+
     # Optimising the rt threshold based on the Training Data.
     cls_d, rt = osnndr.fit(corpus_mtrx[trn_idxs, :], cls_gnr_tgs[trn_idxs])
 
     # Executing predict() and getting the predicted Class Tags. 0 means Unclassified!
     pre_y, pre_r = osnndr.predict(corpus_mtrx[crv_idxs, :])
+
+    print "Eval Passed"
 
     # Returning the results.
     return {
